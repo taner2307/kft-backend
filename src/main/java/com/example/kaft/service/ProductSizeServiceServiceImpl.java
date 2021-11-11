c3929371078fb20b4aa5ada1982880fb0213ff07
Taner Açış <taner.acis@pia-team.com>
2021-11-12 02:16:48 +0300

    init

--- src/main/java/com/example/kaft/service/ProductSizeServiceServiceImpl.java
+++ src/main/java/com/example/kaft/service/ProductSizeServiceServiceImpl.java
@@ -6,9 +6,14 @@ import com.example.kaft.model.SizeTable;
 import com.example.kaft.model.ProductSize;
 import com.example.kaft.repository.SizeTableRepository;
 import lombok.AllArgsConstructor;
+import org.apache.tomcat.util.http.parser.Ranges;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnJava;
+import org.springframework.data.domain.Range;
 import org.springframework.stereotype.Service;
 
+import java.awt.font.NumericShaper;
 import java.util.List;
+import java.util.TreeMap;
 
 @Service
 @AllArgsConstructor
@@ -21,10 +26,10 @@ public class ProductSizeServiceServiceImpl implements IProductSizeService {
         List<SizeTable> sizeTableList = sizeTableRepository.findAll();
         for (SizeTable st : sizeTableList) {
             if (productSize.getGender() == st.getGender() &&
-                    (productSize.getWeight() > st.getBodyWeightMin() &&
-                            productSize.getWeight() < st.getBodyWeightMax()) &&
-                    (productSize.getHeight() > st.getBodyHeightMin() &&
-                            productSize.getHeight() < st.getBodyHeightMax())) {
+                    (productSize.getWeight() >= st.getBodyWeightMin() &&
+                            productSize.getWeight() <= st.getBodyWeightMax()) &&
+                    (productSize.getHeight() >= st.getBodyHeightMin() &&
+                            productSize.getHeight() <= st.getBodyHeightMax())) {
                 productSize.setSizeSuggestion(st.getSize());
             }
         }
@@ -36,21 +41,15 @@ public class ProductSizeServiceServiceImpl implements IProductSizeService {
     }
 
     public Size calculateBMI(Gender gender, double weight, double height) {
-        Size calculatedSize = null;
         double bmi = weight / Math.pow((height / 100), 2);
         bmi = gender == Gender.FEMALE ? bmi + 1.0 : bmi;
-        System.out.println(bmi);
-        if (bmi <= 18.5) {
-            calculatedSize = gender == Gender.FEMALE ? Size.XS : Size.S;
-        } else if (bmi >= 18.6 && bmi <= 24.9) {
-            calculatedSize = gender == Gender.FEMALE ? Size.S : Size.M;
-        } else if (bmi >= 25.0 && bmi <= 29.9) {
-            calculatedSize = gender == Gender.FEMALE ? Size.M : Size.L;
-        } else if (bmi >= 30.0 && bmi <= 31.9) {
-            calculatedSize = gender == Gender.FEMALE ? Size.L : Size.XL;
-        } else if(bmi >= 32) {
-            calculatedSize = Size.XL;
-        }
-        return calculatedSize;
+        TreeMap<Double, Size> gradeMap = new TreeMap<>();
+        gradeMap.put(0.0,gender == Gender.FEMALE ? Size.XS : Size.S);
+        gradeMap.put(18.6,gender == Gender.FEMALE ? Size.S : Size.M);
+        gradeMap.put(25.0,gender == Gender.FEMALE ? Size.M : Size.L);
+        gradeMap.put(30.0,gender == Gender.FEMALE ? Size.L : Size.XL);
+        gradeMap.put(32.0,Size.XL);
+
+        return gradeMap.floorEntry(bmi).getValue();
     }
 }
